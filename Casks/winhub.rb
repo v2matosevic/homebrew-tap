11371cd59e59f3a8774fba7744c471da8cc6bf0c
version2 <matosevic.markom@gmail.com>
2026-06-03 14:45:58 +0200

    winhub 0.5.0

--- Casks/winhub.rb
+++ Casks/winhub.rb
@@ -1,6 +1,6 @@
 cask "winhub" do
-  version "0.4.0"
-  sha256 "7cb3a4c604771ec1b5dc1ef96c14e9345a67513fb5410287cff8a6ce3c1cd58e"
+  version "0.5.0"
+  sha256 "515059f29b7f6ba1d21a107dbe55e6dec0699e111fb5adc7dd8f2d448a229b5d"
 
   url "https://github.com/v2matosevic/WinHub/releases/download/v#{version}/WinHub-#{version}.dmg"
   name "WinHub"
